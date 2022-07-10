f0d871d5f45519ac8bc65802422c9b388726b8a8
claudio <rainclaudio25@gmail.com>
2022-07-10 11:22:43 -0400

    no cambios importantes

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -7,7 +7,6 @@ class ActionMyKB(ActionQueryKnowledgeBase):
         # load knowledge base with data from the given file
         knowledge_base = InMemoryKnowledgeBase("knowledge_base_data.json")
 
-<<<<<<< HEAD
 # from typing import Any, Text, Dict, List
 #
 # from rasa_sdk import Action, Tracker
@@ -39,12 +38,6 @@ class ActionMyKB(ActionQueryKnowledgeBase):
         # by default the representation function is just the name of the object
         knowledge_base.set_representation_function_of_object(
             "hotel", lambda obj: obj["name"] + " (" + obj["city"] + ")"
-=======
-        # overwrite the representation function of the hotel object
-        # by default the representation function is just the name of the object
-        knowledge_base.set_representation_function_of_object(
-            "producto", lambda obj: obj["name"]
->>>>>>> bbf34e67692468b012ec66cfad46e67fd4c1f0d2
         )
 
         super().__init__(knowledge_base)
