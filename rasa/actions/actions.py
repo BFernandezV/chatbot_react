6ceb45376358e39faf7704900dfccdd64ff2d143
claudio <rainclaudio25@gmail.com>
2022-07-12 16:30:32 -0400

    frutas

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -14,6 +14,7 @@ from rasa_sdk.executor import CollectingDispatcher
 # https://github.com/thedanelias/rock-paper-scissors-python/blob/master/rockpaperscissors.py, MIT liscence
  
 class ActionPlayRPS(Action):
+
    
     def name(self) -> Text:
         return "action_play_rps"
@@ -55,3 +56,36 @@ class ActionPlayRPS(Action):
             dispatcher.utter_message(text="It was a tie!")
  
         return []
+
+
+class ActionResponseProduct(Action):
+   
+    def name(self) -> Text:
+        return "action_response_product"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+ 
+        # producto todo con minusculas
+        user_ask_product = tracker.get_slot("producto")
+        user_ask_product = user_ask_product.lower()
+        dispatcher.utter_message(text=f"Has preguntado por {user_ask_product}")
+        
+        if user_ask_product == "carne":
+            dispatcher.utter_message(text="El pasillo de las Carnes es el pasillo 8")
+        elif user_ask_product == "fruta":
+            dispatcher.utter_message(text="El pasillo de las frutas es el pasillo 6")
+        elif user_ask_product == "lacteo":
+            dispatcher.utter_message(text="El pasillo de los Lacteos es el pasillo 3")
+        elif user_ask_product == "aseo personal":
+            dispatcher.utter_message(text="El pasillo del aseo personal es el pasillo 5")
+        elif user_ask_product == "reposteria":
+            dispatcher.utter_message(text="El pasillo de la repostería es el pasillo")
+        elif user_ask_product == "ropa":
+            dispatcher.utter_message(text="El pasillo de la ropa es el pasillo 6")
+        else:
+            dispatcher.utter_message(text="No he encontrado el producto que estas buscando")
+ 
+        return []
+   
