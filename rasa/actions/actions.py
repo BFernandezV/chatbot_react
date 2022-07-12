fdb84bfa02713f482b60189f8911fda5bd97108d
claudio <rainclaudio25@gmail.com>
2022-07-12 09:53:53 -0400

    cambio martes por la mañana

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -1,43 +1,57 @@
-from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
-from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
-
-
-class ActionMyKB(ActionQueryKnowledgeBase):
-    def __init__(self):
-        # load knowledge base with data from the given file
-        knowledge_base = InMemoryKnowledgeBase("knowledge_base_data.json")
-
-# from typing import Any, Text, Dict, List
+# This files contains your custom actions which can be used to run
+# custom Python code.
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
-from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
-
-
-class ActionMyKB(ActionQueryKnowledgeBase):
-    def __init__(self):
-        # load knowledge base with data from the given file
-        knowledge_base = InMemoryKnowledgeBase("knowledge_base_data.json")
-
-        # overwrite the representation function of the hotel object
-        # by default the representation function is just the name of the object
-        knowledge_base.set_representation_function_of_object(
-            "hotel", lambda obj: obj["name"] + " (" + obj["city"] + ")"
-        )
-
-        super().__init__(knowledge_base)
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+ 
+from typing import Any, Text, Dict, List
+import random
+ 
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+ 
+# computer_choice & determine_winner functions refactored from
+# https://github.com/thedanelias/rock-paper-scissors-python/blob/master/rockpaperscissors.py, MIT liscence
+ 
+class ActionPlayRPS(Action):
+   
+    def name(self) -> Text:
+        return "action_play_rps"
+ 
+    def computer_choice(self):
+        generatednum = random.randint(1,3)
+        if generatednum == 1:
+            computerchoice = "rock"
+        elif generatednum == 2:
+            computerchoice = "paper"
+        elif generatednum == 3:
+            computerchoice = "scissors"
+       
+        return(computerchoice)
+ 
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+ 
+        # play rock paper scissors
+        user_choice = tracker.get_slot("choice")
+        dispatcher.utter_message(text=f"You chose {user_choice}")
+        comp_choice = self.computer_choice()
+        dispatcher.utter_message(text=f"The computer chose {comp_choice}")
+ 
+        if user_choice == "rock" and comp_choice == "scissors":
+            dispatcher.utter_message(text="Congrats, you won!")
+        elif user_choice == "rock" and comp_choice == "paper":
+            dispatcher.utter_message(text="The computer won this round.")
+        elif user_choice == "paper" and comp_choice == "rock":
+            dispatcher.utter_message(text="Congrats, you won!")
+        elif user_choice == "paper" and comp_choice == "scissors":
+            dispatcher.utter_message(text="The computer won this round.")
+        elif user_choice == "scissors" and comp_choice == "paper":
+            dispatcher.utter_message(text="Congrats, you won!")
+        elif user_choice == "scissors" and comp_choice == "rock":
+            dispatcher.utter_message(text="The computer won this round.")
+        else:
+            dispatcher.utter_message(text="It was a tie!")
+ 
+        return []
