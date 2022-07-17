85e9fc146a72e08ed2ee76b3d053a7cf5b17e65e
Benjamin <danilmunoz@inf.udec.cl>
2022-07-17 17:00:02 -0400

    no message

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -7,44 +7,10 @@ class ActionMyKB(ActionQueryKnowledgeBase):
         # load knowledge base with data from the given file
         knowledge_base = InMemoryKnowledgeBase("knowledge_base_data.json")
 
-<<<<<<< HEAD
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
-from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
-
-
-class ActionMyKB(ActionQueryKnowledgeBase):
-    def __init__(self):
-        # load knowledge base with data from the given file
-        knowledge_base = InMemoryKnowledgeBase("knowledge_base_data.json")
-
-        # overwrite the representation function of the hotel object
-        # by default the representation function is just the name of the object
-        knowledge_base.set_representation_function_of_object(
-            "hotel", lambda obj: obj["name"] + " (" + obj["city"] + ")"
-=======
         # overwrite the representation function of the hotel object
         # by default the representation function is just the name of the object
         knowledge_base.set_representation_function_of_object(
             "producto", lambda obj: obj["name"]
->>>>>>> bbf34e67692468b012ec66cfad46e67fd4c1f0d2
         )
 
         super().__init__(knowledge_base)
