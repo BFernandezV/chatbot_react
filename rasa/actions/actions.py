9d2ba17f1dc45a61dd2e869f2138bad90c69a87c
claudio <rainclaudio25@gmail.com>
2022-07-17 11:29:07 -0400

    aun no saco las lookup

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -69,20 +69,19 @@ class ActionResponseProduct(Action):
  
         # producto todo con minusculas
         user_ask_product = tracker.get_slot("producto")
-        user_ask_product = user_ask_product.lower()
         dispatcher.utter_message(text=f"Has preguntado por {user_ask_product}")
         
-        if user_ask_product == "carne":
+        if user_ask_product == "Carne":
             dispatcher.utter_message(text="El pasillo de las Carnes es el pasillo 8")
-        elif user_ask_product == "fruta":
+        elif user_ask_product == "Fruta":
             dispatcher.utter_message(text="El pasillo de las frutas es el pasillo 6")
-        elif user_ask_product == "lacteo":
+        elif user_ask_product == "Lacteo":
             dispatcher.utter_message(text="El pasillo de los Lacteos es el pasillo 3")
-        elif user_ask_product == "aseo personal":
+        elif user_ask_product == "Aseo personal":
             dispatcher.utter_message(text="El pasillo del aseo personal es el pasillo 5")
-        elif user_ask_product == "reposteria":
+        elif user_ask_product == "Reposteria":
             dispatcher.utter_message(text="El pasillo de la repostería es el pasillo")
-        elif user_ask_product == "ropa":
+        elif user_ask_product == "Ropa":
             dispatcher.utter_message(text="El pasillo de la ropa es el pasillo 6")
         else:
             dispatcher.utter_message(text="No he encontrado el producto que estas buscando")
