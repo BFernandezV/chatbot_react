454e4076cf601e22b9ace602a34082cfcb743dee
claudio <rainclaudio25@gmail.com>
2022-07-18 22:17:27 -0400

    refactorizando

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -19,49 +19,6 @@ from rasa_sdk.executor import CollectingDispatcher
 # computer_choice & determine_winner functions refactored from
 # https://github.com/thedanelias/rock-paper-scissors-python/blob/master/rockpaperscissors.py, MIT liscence
  
-class ActionPlayRPS(Action):
-
-   
-    def name(self) -> Text:
-        return "action_play_rps"
- 
-    def computer_choice(self):
-        generatednum = random.randint(1,3)
-        if generatednum == 1:
-            computerchoice = "rock"
-        elif generatednum == 2:
-            computerchoice = "paper"
-        elif generatednum == 3:
-            computerchoice = "scissors"
-       
-        return(computerchoice)
- 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
- 
-        # play rock paper scissors
-        user_choice = tracker.get_slot("choice")
-        dispatcher.utter_message(text=f"You chose {user_choice}")
-        comp_choice = self.computer_choice()
-        dispatcher.utter_message(text=f"The computer chose {comp_choice}")
- 
-        if user_choice == "rock" and comp_choice == "scissors":
-            dispatcher.utter_message(text="Congrats, you won!")
-        elif user_choice == "rock" and comp_choice == "paper":
-            dispatcher.utter_message(text="The computer won this round.")
-        elif user_choice == "paper" and comp_choice == "rock":
-            dispatcher.utter_message(text="Congrats, you won!")
-        elif user_choice == "paper" and comp_choice == "scissors":
-            dispatcher.utter_message(text="The computer won this round.")
-        elif user_choice == "scissors" and comp_choice == "paper":
-            dispatcher.utter_message(text="Congrats, you won!")
-        elif user_choice == "scissors" and comp_choice == "rock":
-            dispatcher.utter_message(text="The computer won this round.")
-        else:
-            dispatcher.utter_message(text="It was a tie!")
- 
-        return []
 
 def cleanText(text):
     # print("CLEAN TEXT: ", text)
@@ -85,75 +42,26 @@ def cleanText(text):
     elif(text.find("buscando ") >= 0):
         return text.replace("buscando ", "")
     return "Producto desconocido"
-class ActionResponseProduct(Action):
 
+
+class ActionAnswerProduct(Action):
     def name(self) -> Text:
-        return "action_response_product"
-    
+        return "action_answer_product"
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
  
         # producto todo con minusculas
-        user_ask_product = tracker.get_slot("name_product")
-        print("Este es el domain",domain)
-        print("Este es el producto sin filtro: ", user_ask_product)
-        if(user_ask_product != None): 
-            user_ask_product = cleanText(user_ask_product.lower())
-        else:
-            dispatcher.utter_message(text="No te he entendido :c...repite la frase")
+        account_number = tracker.get_slot("producto")
+        # print("ORIGINAL:", account_number)
 
-        # user_ask_product = user_ask_product.lower()
-
-        # es = wn.Wordnet('omw-es:1.4')
-        # response = es.words(user_ask_product)[0]
-        # print("PALABRA: ", response)
-        # print(response)
-        # print(response.pos)
-        # print(response.forms())
-        # print(response.lemma())
-        # # print(response.derivated_words())
-        # print(response.senses())
-        # print(response.synsets())
-        # print("------------------------------------------------")
-        
-        if user_ask_product == "Carne":
-            dispatcher.utter_message(text="El pasillo de las Carnes es el pasillo 8")
-        elif user_ask_product == "Fruta":
-            dispatcher.utter_message(text="El pasillo de las frutas es el pasillo 6")
-        elif user_ask_product == "Lacteo":
-            dispatcher.utter_message(text="El pasillo de los Lacteos es el pasillo 3")
-        elif user_ask_product == "Aseo personal":
-            dispatcher.utter_message(text="El pasillo del aseo personal es el pasillo 5")
-        elif user_ask_product == "Reposteria":
-            dispatcher.utter_message(text="El pasillo de la repostería es el pasillo")
-        elif user_ask_product == "Ropa":
-            dispatcher.utter_message(text="El pasillo de la ropa es el pasillo 6")
-        elif user_ask_product == "platano":
-            dispatcher.utter_message(text="El platano es muy rico ñam ñam")
-        else:
-            dispatcher.utter_message(text="No he encontrado el producto que estas buscando")
-        
-        return "NADA"
-
-
-class ActionGetAccountNumber(Action):
-    def name(self) -> Text:
-        return "action_account_number"
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
- 
-        # producto todo con minusculas
-        account_number = tracker.get_slot("account_number")
-        print("ORIGINAL:", account_number)
         if(account_number != None): 
             account_number = cleanText(account_number.lower())
 
         spell = Speller('es')
         word = spell(account_number)
-        print("CORRECTOR: ",word)
-        print("------------------------------------------")
+        # print("CORRECTOR: ",word)
+        # print("------------------------------------------")
 
         file = open('knowledge_base_data.json')
         data = json.load(file)
@@ -182,63 +90,3 @@ class ActionGetAccountNumber(Action):
                 
 
        
-
-class ActionResponderProducto(Action):
-
-   
-    def name(self) -> Text:
-        return "action_responder_producto"
-    
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
- 
-        # producto todo con minusculas
-        user_ask_product = tracker.get_slot("producto_string")
-        print("PRODUCTO IDENTIFICADO: ",user_ask_product)
-        # user_ask_product = user_ask_product.lower()
-        dispatcher.utter_message(text=f"Has preguntado por {user_ask_product}")
-
-        es = wn.Wordnet('omw-es:1.4')
-        response = es.words(user_ask_product)[0]
-        print("PALABRA: ", response)
-        print(response)
-        print(response.pos)
-        print(response.forms())
-        print(response.lemma())
-        # print(response.derivated_words())
-        print(response.senses())
-        print(response.synsets())
-        print("------------------------------------------------")
-        
-        if user_ask_product == "Carne":
-            dispatcher.utter_message(text="El pasillo de las Carnes es el pasillo 8")
-        elif user_ask_product == "Fruta":
-            dispatcher.utter_message(text="El pasillo de las frutas es el pasillo 6")
-        elif user_ask_product == "Lacteo":
-            dispatcher.utter_message(text="El pasillo de los Lacteos es el pasillo 3")
-        elif user_ask_product == "Aseo personal":
-            dispatcher.utter_message(text="El pasillo del aseo personal es el pasillo 5")
-        elif user_ask_product == "Reposteria":
-            dispatcher.utter_message(text="El pasillo de la repostería es el pasillo")
-        elif user_ask_product == "Ropa":
-            dispatcher.utter_message(text="El pasillo de la ropa es el pasillo 6")
-        elif user_ask_product == "platano":
-            dispatcher.utter_message(text="El platano es muy rico ñam ñam")
-        else:
-            dispatcher.utter_message(text="No he encontrado el producto que estas buscando")
- 
-        return []
-
-
-#   word = spell(account_number)
-
-#     es = wn.Wordnet('omw-es:1.4')
-
-#     for x in es.synsets(word):
-#         print("Esta es la palabra: ", x)
-#         print("Estos son los similares: ", x.lemmas())
-#         print("Hipernimos: ", x.hypernyms())
-#         print("Hipernimos value: ", x.hyponyms()[1].lemmas())
-#         print("Hiponimos: ", x.hyponyms())
-#         print("Hiponimos: ", x.hyponyms()[0].lemmas())
